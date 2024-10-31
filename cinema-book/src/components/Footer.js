6f82867f072f7aa6443903a6a456e907e1fd9104
asifbhuiyann <getconnectwithasif@gmail.com>
2024-10-31 10:59:17 +0600

    footer update, added links

--- cinema-book/src/components/Footer.js
+++ cinema-book/src/components/Footer.js
@@ -5,17 +5,19 @@
         <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400"> <Link to="/" className="hover:underline">CinemaBook</Link>. All Rights Reserved.</span>
             <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+
                 <li>
-                    <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
+                    <a href="https://www.facebook.com/aasifbhuiyan/" target="_blank" className="mr-4 hover:underline md:mr-6">Facebook</a>
                 </li>
                 <li>
-                    <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
+                    <a href="https://x.com/asif__bhuiyan" target="_blank" className="mr-4 hover:underline md:mr-6">Twitter</a>
                 </li>
                 <li>
-                    <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">Youtube</a>
+                    <a href="https://www.linkedin.com/in/aasif-bhuiyan/" target="_blank" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
                 </li>
+
                 <li>
-                    <a href="#" target="_blank" className="hover:underline">Github</a>
+                    <a href="https://github.com/asifbhuiyann" target="_blank" className="hover:underline">Github</a>
                 </li>
             </ul>
         </footer>
